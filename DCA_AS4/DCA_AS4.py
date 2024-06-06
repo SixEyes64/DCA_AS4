02ca5da46b7e56ddc24f5d8d24a38ba55df73721
2022001566 <enderscarr@gmail.com>
2024-06-06 14:17:56 +1200

    print_positive_growth function is working

--- DCA_AS4/DCA_AS4.py
+++ DCA_AS4/DCA_AS4.py
@@ -7,14 +7,13 @@ Pledge of Honour: I pledge by honour that this program is solely my work
 Purpose: Program that reads MS Access content and queries data for statistics
 '''
 
-
+HeaderFormat = '{0:<8}{1:<25}{2:<20}{3:<18}{4:<18}{5:20}{6:<20}{7:10}'
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2022001566\source\repos\DCA_AS4\DCA_AS4\company.accdb')
+cursor = conn.cursor()
 
 def print_all_records():
     '''Prints all records'''
-    HeaderFormat = '{0:<8}{1:<25}{2:<20}{3:<18}{4:<18}{5:20}{6:<20}{7:10}'
     
-    conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\ender\OneDrive\Documents\WITT\4DCA\Assignments\Assessment_4\DCA_Assessment4\company.accdb')
-    cursor = conn.cursor()
 
     cursor.execute('SELECT * FROM Company_Data')
 
@@ -27,22 +26,39 @@ def print_all_records():
         record = f'{row.ID:<8}{row.company_name:<25}{row.industry:<25}{row.year_revenue:<15}{row.revenue_growth:<16}{row.number_of_employees:<20}{row.headquarter:<20}{date:<10}'
         print(record)
 
+
 def print_positive_growth():
     '''Only prints data with positive growth'''
-    pass
+    cursor.execute('SELECT * FROM Company_Data WHERE revenue_growth > 0')
+    
+    print(HeaderFormat.format('ID','Company Name','Industry','Year Revenue','Revenue Growth','# of Employees','Headquarters','Company Found Date'))
+    print(f'{"_":_<150}')
+    
+    for row in cursor.fetchall():
+        date = datetime.strftime(row.company_found_date, '%d/%m/%Y')
+
+        record = f'{row.ID:<8}{row.company_name:<25}{row.industry:<25}{row.year_revenue:<15}{row.revenue_growth:<16}{row.number_of_employees:<20}{row.headquarter:<20}{date:<10}'
+        print(record)
+
+
 
 def query_record_by_date():
     '''Displays a query sorting records by date'''
     pass
+
 
 def count_companies_between_dates():
     '''Asks user for start and end date, and then lists companies 
        between those two dates'''
     pass
 
+
+
 def main():
     '''Runs all the main functions for the program to function'''
     pass
 
-print_all_records()
+
+# print_all_records()
+print_positive_growth()
 
